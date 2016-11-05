11c681db6f2c98ea51e640471fd17faa7b64dddf
Bastiaan Bakker <bbakker@xebia.com>
2016-11-05 23:23:36 +0100

    remove nomad+consul variants of startup scripts remove install scripts for nomad servers in managed instance group

--- script/install-servers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcloud compute instance-templates create "server" --machine-type "f1-micro" --image-family "/coreos-cloud/coreos-stable" --boot-disk-size "10GB" --metadata-from-file startup-script=server-script
-gcloud beta compute instance-groups managed create "nomad-eu" --region "europe-west1" --template "server" --size "3"
